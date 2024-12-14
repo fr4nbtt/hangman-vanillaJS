b8cd15ccde90e6c4bce46e107e15be0a552dd5c3
fr4nbtt <batet.francisco@gmail.com>
2024-12-14 20:49:16 -0300

    Update wrong letters display and style; Update popup style

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function updateWrongLettersEl() {
   // Display wrong letters
   wrongLettersEl.innerHTML = `
         ${wrongLetters.length > 0 ? `<p>Wrong Letters:</p>` : ""}
-        ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+        ${wrongLetters.map((letter) => `<span>${letter}</span>`).join("")}
     `;
 
   // Display hangman parts
